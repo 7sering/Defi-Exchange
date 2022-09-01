5019567a8663b5e6bb4669bb1044c755a2f483c7
7sering <codenow8@gmail.com>
2022-09-01 20:11:18 +0545

    Add Liquidity To Contract

--- hardhat/contracts/Exchange.sol
+++ hardhat/contracts/Exchange.sol
@@ -12,8 +12,36 @@ constructor(address _CryptoDevtoken) ERC2("CryptoDev LP Token", "CDLP"){
     cryptoDevTokenAddress = _CryptoDevtoken;
 }
 
+// Get Reserve Balance of Contract
 function getReserve() public view returns (uint) {
     return ERC20(cryptoDevTokenAddress).balanceOf(address(this));
+}
+
+// Adds Liquidity  to the exchange.
+function addLiquidity(uint _amount) public payable returns(uint){
+    uint liquidity;
+    uint ethBalance = address(this).balance;
+    uint cryptoDevTokenReserve = getReserve();
+    ERC20 cryptoDevToken = ERC20(cryptoDevTokenAddress);
+
+        if(cryptoDevTokenReserve == 0) {
+        cryptoDevToken.transferFrom(msg.sender, address(this), _amount);
+
+        liquidity = ethBalance;
+        _mint(msg.sender, liquidity);
+    } else {
+
+        uint ethReserve =  ethBalance - msg.value;
+     
+        uint cryptoDevTokenAmount = (msg.value * cryptoDevTokenReserve)/(ethReserve);
+        require(_amount >= cryptoDevTokenAmount, "Amount of tokens sent is less than the minimum tokens required");
+  
+        cryptoDevToken.transferFrom(msg.sender, address(this), cryptoDevTokenAmount);
+   
+        liquidity = (totalSupply() * msg.value)/ ethReserve;
+        _mint(msg.sender, liquidity);
+    }
+     return liquidity;
 }
 
 }
